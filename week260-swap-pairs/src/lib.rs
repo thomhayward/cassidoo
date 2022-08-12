54b9ed7aed0e22242c5899482262efb9338d31f5
Thom <14987462+thomhayward@users.noreply.github.com>
2022-08-12 18:38:19 +0100

    solutions to week 260

--- week260-swap-pairs/src/lib.rs
+++ week260-swap-pairs/src/lib.rs
@@ -1,33 +1,7 @@
-
-pub fn swap_pairs<T>(s: &mut [T]) {
-    for pair in s.chunks_exact_mut(2) {
-        pair.swap(0, 1);
-    }
-}
-
-#[ cfg(test)]
-mod test {
-
-    use super::swap_pairs;
-
-    #[test]
-    fn example() {
-        let mut ex = [1, 2, 3, 4];
-        swap_pairs(&mut ex);
-        assert_eq!(
-            ex,
-            [2, 1, 4, 3]
-        );
-    }
-
-    #[test]
-    fn empty() {
-         let mut empty: Vec<u32> = vec![];
-         swap_pairs(&mut empty);
-         assert_eq!(
-            empty,
-            []
-         );
-    }
-
-}
+// Week 260
+//
+// "__Given a list, swap every two adjacent nodes.__ Something to think about: How would your code
+// change if this were a linked list, versus an array?"\
+// 
+pub mod iters;
+pub mod slice;
