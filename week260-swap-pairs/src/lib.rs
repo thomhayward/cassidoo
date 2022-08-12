83ce4aca4de8ea3119e316a1022c2ceceb01c923
Thom <14987462+thomhayward@users.noreply.github.com>
2022-08-12 18:49:36 +0100

    doc

--- week260-swap-pairs/src/lib.rs
+++ week260-swap-pairs/src/lib.rs
@@ -1,7 +1,23 @@
-// Week 260
-//
-// "__Given a list, swap every two adjacent nodes.__ Something to think about: How would your code
-// change if this were a linked list, versus an array?"\
-// 
-pub mod iters;
+//! Week 260
+//!
+//! "__Given a list, swap every two adjacent nodes.__ Something to think about: How would your code
+//! change if this were a linked list, versus an array?"\
+//!
+//! Two solutions to this week's problem; one that only works on `slice`s and one that can consume any iterable.
+//! 
+/// Easy mode: `slice::SwapPairs`
+/// 
+/// - Swaps pairs in any `&mut [T]` in-place.
+/// - No restrictions on `T` (doesn't need `Copy` or `Clone`).
+/// - Simplest implementation, and no memory overhead!
+/// - Only works on sliceable types.
+/// 
 pub mod slice;
+///
+/// Hard Mode: `iters::SwapPairs`
+/// 
+/// - Works on anything that can be coverted into an `Iterator` with `IntoIterator`.
+/// - No restrictions on `T`.
+/// - Streamable (can work with non-terminating iterators).
+/// 
+pub mod iters;
